41e82a7b29c7788db699d7df4ffee2a24cddf86e
xieguigang <xie.guigang@gcmodeller.org>
2020-04-05 00:53:56 +0800

    code style improvements of the script parser

--- SSystem/Script/ScriptText/ScriptParser.vb
+++ SSystem/Script/ScriptText/ScriptParser.vb
@@ -52,6 +52,120 @@ Namespace Script
 
     Public Module ScriptParser
 
+        <Extension>
+        Private Function getScriptTokenGroups(script As String()) As Dictionary(Of ScriptTokens, ScriptToken())
+            Dim tokens As ScriptToken() = TokenIcer.TryParse(script)
+            Dim tokenGroups As Dictionary(Of ScriptTokens, ScriptToken()) =
+                (From x As ScriptToken
+                 In tokens
+                 Select x
+                 Group x By x.name Into Group) _
+                     .ToDictionary(Function(x) x.name,
+                                   Function(x)
+                                       Return x.Group.ToArray
+                                   End Function)
+            Return tokenGroups
+        End Function
+
+        <Extension>
+        Private Function setConstants(env As ExpressionEngine, tokens As Dictionary(Of ScriptTokens, ScriptToken())) As NamedValue(Of String)()
+            Dim c As NamedValue(Of String)()
+
+            If tokens.ContainsKey(ScriptTokens.Constant) Then
+                c = tokens(ScriptTokens.Constant) _
+                    .Select(AddressOf ModelParsers.ConstantParser) _
+                    .ToArray
+            Else
+                c = {}
+            End If
+
+            For Each x As NamedValue(Of String) In c
+                Call env.SetSymbol(x.Name, x.Value)
+            Next
+
+            Return c
+        End Function
+
+        <Extension>
+        Private Function getTimeFinalConfig(tokens As Dictionary(Of ScriptTokens, ScriptToken()), env As ExpressionEngine) As Integer
+            If Not tokens.ContainsKey(ScriptTokens.Time) Then
+                Return 100
+            Else
+                Return tokens(ScriptTokens.Time) _
+                    .First _
+                    .DoCall(Function(t) env.Evaluate(t.text))
+            End If
+        End Function
+
+        <Extension>
+        Private Function getTitleOrDefault(tokens As Dictionary(Of ScriptTokens, ScriptToken())) As String
+            If Not tokens.ContainsKey(ScriptTokens.Title) Then
+                Return "UNNAMED TITLE"
+            Else
+                Return tokens(ScriptTokens.Title).First.text
+            End If
+        End Function
+
+        <Extension>
+        Private Function getCommentText(tokens As Dictionary(Of ScriptTokens, ScriptToken())) As String
+            If tokens.ContainsKey(ScriptTokens.Comment) Then
+                Return tokens(ScriptTokens.Comment) _
+                    .Select(Function(x) x.text) _
+                    .JoinBy(vbCrLf)
+            Else
+                Return ""
+            End If
+        End Function
+
+        <Extension>
+        Private Function getUserFunctionOrDefault(tokens As Dictionary(Of ScriptTokens, ScriptToken())) As [Function]()
+            If tokens.ContainsKey(ScriptTokens.Function) Then
+                Return tokens(ScriptTokens.Function) _
+                    .Select(Function(x) CType(x.text, [Function])) _
+                    .ToArray
+            Else
+                Return {}
+            End If
+        End Function
+
+        <Extension>
+        Private Sub setAliasNames(model As Model, tokens As Dictionary(Of ScriptTokens, ScriptToken()))
+            Dim nameList As String()
+            Dim name As String
+
+            If tokens.ContainsKey(ScriptTokens.Alias) Then
+                nameList = tokens(ScriptTokens.Alias) _
+                    .Select(Function(x) x.text) _
+                    .ToArray
+            Else
+                nameList = {}
+            End If
+
+            For Each s As String In nameList
+                s = Mid(s, 7)
+                name = s.Split.First
+                model.FindObject(s).title = Mid(s, Len(name) + 2)
+            Next
+        End Sub
+
+        <Extension>
+        Private Sub setObjectComments(model As Model, tokens As Dictionary(Of ScriptTokens, ScriptToken()))
+            Dim comments As String()
+            Dim name As String
+
+            If tokens.ContainsKey(ScriptTokens.SubsComments) Then
+                comments = tokens(ScriptTokens.SubsComments).Select(Function(x) x.text).ToArray
+            Else
+                comments = {}
+            End If
+
+            For Each s As String In comments
+                s = Mid(s, 9)
+                name = s.Split.First
+                model.FindObject(name).comment = Mid(s, Len(name) + 2)
+            Next
+        End Sub
+
         ''' <summary>
         ''' 
         ''' </summary>
@@ -60,95 +174,42 @@ Namespace Script
         ''' 
         <Extension>
         Public Function ParseScript(scriptText As String) As Model
-            Dim tokens As ScriptToken() = TokenIcer.TryParse(scriptText.LineTokens)
-            Dim typeTokens = (From x As ScriptToken
-                              In tokens
-                              Select x
-                              Group x By x.name Into Group) _
-                                   .ToDictionary(Function(x) x.name,
-                                                 Function(x)
-                                                     Return x.Group.ToArray
-                                                 End Function)
-
-            Dim equations = typeTokens(Script.ScriptTokens.Reaction).Select(AddressOf sEquationParser)
-            Dim Disturbs As Experiment()
-            Dim FinalTime As Integer
+            Dim tokens = scriptText.LineTokens.getScriptTokenGroups
+            Dim equations = tokens(ScriptTokens.Reaction) _
+                .Select(AddressOf sEquationParser) _
+                .ToArray
+            Dim title As String = tokens.getTitleOrDefault
+            Dim disturbs As Experiment()
             Dim val As New ExpressionEngine
+            Dim finalTime As Integer = tokens.getTimeFinalConfig(val)
+            Dim c As NamedValue(Of String)() = val.setConstants(tokens)
+            Dim inits = tokens(ScriptTokens.InitValue) _
+                .Select(Function(x) var.TryParse(x.text, val)) _
+                .ToArray
 
-            Dim c =
-                If(typeTokens.ContainsKey(Script.ScriptTokens.Constant),
-                typeTokens(Script.ScriptTokens.Constant).Select(AddressOf ModelParsers.ConstantParser),
-                {})
-
-            For Each x As NamedValue(Of String) In c
-                Call val.SetSymbol(x.Name, x.Value)
-            Next
-
-            Dim inits = typeTokens(Script.ScriptTokens.InitValue).Select(Function(x) var.TryParse(x.Text, val))
-
-            If typeTokens.ContainsKey(Script.ScriptTokens.Disturb) Then
-                Disturbs = typeTokens(Script.ScriptTokens.Disturb).Select(Function(x) ExperimentParser(x.Text))
+            If tokens.ContainsKey(ScriptTokens.Disturb) Then
+                disturbs = tokens(ScriptTokens.Disturb) _
+                    .Select(Function(x)
+                                Return ExperimentParser(x.text)
+                            End Function) _
+                    .ToArray
             Else
-                Disturbs = {}
+                disturbs = {}
             End If
-
-            If Not typeTokens.ContainsKey(Script.ScriptTokens.Time) Then
-                FinalTime = 100
-            Else
-                FinalTime = val.Evaluate(typeTokens(Script.ScriptTokens.Time).First.Text)
-            End If
-
-            Dim Title As String
-
-            If Not typeTokens.ContainsKey(Script.ScriptTokens.Title) Then
-                Title = "UNNAMED TITLE"
-            Else
-                Title = typeTokens(Script.ScriptTokens.Title).First.Text
-            End If
-
-            Dim Comments As String() =
-                If(typeTokens.ContainsKey(Script.ScriptTokens.Comment),
-                typeTokens(Script.ScriptTokens.Comment).Select(Function(x) x.Text),
-                {})
 
             Dim model As New Model With {
                 .sEquations = equations,
                 .Vars = inits,
-                .Experiments = Disturbs,
-                .Comment = Comments.JoinBy(vbCrLf),
-                .FinalTime = FinalTime,
-                .Title = Title,
-                .Constant = c
+                .Experiments = disturbs,
+                .Comment = tokens.getCommentText,
+                .FinalTime = finalTime,
+                .Title = title,
+                .Constant = c,
+                .UserFunc = tokens.getUserFunctionOrDefault
             }
-            Dim NameList As String()
 
-            If typeTokens.ContainsKey(Script.ScriptTokens.Alias) Then
-                NameList = typeTokens(Script.ScriptTokens.Alias).Select(Function(x) x.Text)
-            Else
-                NameList = {}
-            End If
-
-            For Each s As String In NameList
-                s = Mid(s, 7)
-                Dim Name As String = s.Split.First
-                model.FindObject(s).Title = Mid(s, Len(Name) + 2)
-            Next
-
-            Dim sc As IEnumerable(Of String) =
-                If(typeTokens.ContainsKey(Script.ScriptTokens.SubsComments),
-                typeTokens(Script.ScriptTokens.SubsComments).Select(Function(x) x.Text),
-                {})
-
-            For Each s As String In sc
-                s = Mid(s, 9)
-                Dim Name As String = s.Split.First
-                model.FindObject(Name).Comment = Mid(s, Len(Name) + 2)
-            Next
-
-            model.UserFunc =
-                If(typeTokens.ContainsKey(Script.ScriptTokens.Function),
-                typeTokens(Script.ScriptTokens.Function).Select(Function(x) CType(x.Text, [Function])),
-                {})
+            Call model.setAliasNames(tokens)
+            Call model.setObjectComments(tokens)
 
             Return model
         End Function
