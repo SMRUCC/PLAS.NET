4b53777228901b3601371ae6c10b588838cce200
xieguigang <xie.guigang@gcmodeller.org>
2020-04-05 00:30:38 +0800

    model parsers

--- SSystem/Script/ScriptText/ScriptParser.vb
+++ SSystem/Script/ScriptText/ScriptParser.vb
@@ -44,7 +44,6 @@
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
-Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math.Scripting.MathExpression
 Imports SMRUCC.genomics.Analysis.SSystem.Kernel.ObjectModels
@@ -52,53 +51,6 @@ Imports SMRUCC.genomics.Analysis.SSystem.Kernel.ObjectModels
 Namespace Script
 
     Public Module ScriptParser
-
-        ''' <summary>
-        ''' 解析系统方程表达式
-        ''' </summary>
-        ''' <param name="x"></param>
-        ''' <returns></returns>
-        Public Function sEquationParser(x As ScriptToken) As SEquation
-            Dim value = x.text.GetTagValue("=")
-            Return New SEquation With {
-                .x = value.Name,
-                .Expression = value.Value
-            }
-        End Function
-
-        ''' <summary>
-        ''' 解析出系统的状态扰动实验表达式
-        ''' </summary>
-        ''' <param name="line"></param>
-        ''' <returns></returns>
-        Public Function ExperimentParser(line As String) As Experiment
-            Dim Tokens As String() = line.Split
-            Dim Dict As New Dictionary(Of String, String)
-            Dim Disturb As New Experiment
-
-            For i As Integer = 1 To Tokens.Length - 2 Step 2
-                Dict.Add(Tokens(i), Tokens(i + 1))
-            Next
-
-            Disturb.Start = Val(Dict("START"))
-            Disturb.Interval = Val(Dict("INTERVAL"))
-            Disturb.Kicks = Val(Dict("KICKS"))
-            Disturb.Id = Dict("OBJECT")
-            Dim Value As String = Dict("VALUE")
-
-            If InStr(Value, "++") = 1 Then
-                Disturb.DisturbType = Types.Increase
-                Disturb.Value = Val(Mid(Value, 3))
-            ElseIf InStr(Value, "--") = 1 Then
-                Disturb.DisturbType = Types.Decrease
-                Disturb.Value = Val(Mid(Value, 3))
-            Else
-                Disturb.DisturbType = Types.ChangeTo
-                Disturb.Value = Val(Value)
-            End If
-
-            Return Disturb
-        End Function
 
         ''' <summary>
         ''' 
@@ -125,7 +77,7 @@ Namespace Script
 
             Dim c =
                 If(typeTokens.ContainsKey(Script.ScriptTokens.Constant),
-                typeTokens(Script.ScriptTokens.Constant).Select(AddressOf ScriptParser.ConstantParser),
+                typeTokens(Script.ScriptTokens.Constant).Select(AddressOf ModelParsers.ConstantParser),
                 {})
 
             For Each x As NamedValue(Of String) In c
@@ -214,33 +166,6 @@ Namespace Script
         <Extension>
         Public Function ParseFile(path As String) As Model
             Return ParseScript(path.ReadAllText)
-        End Function
-
-        ''' <summary>
-        ''' 函数会自动去除掉表达式末尾的注释
-        ''' </summary>
-        ''' <param name="expr"></param>
-        ''' <returns></returns>
-        Public Function ConstantParser(expr As Value(Of String)) As NamedValue(Of String)
-            Dim name As String = (expr = (+expr).Trim).Split.First
-            expr.Value = Mid(expr.Value, name.Length + 1).Trim
-            expr = expr.Value _
-                .GetTagValue("#", failureNoName:=False).Name _
-                .GetTagValue("'", failureNoName:=False).Name _
-                .GetTagValue("//", failureNoName:=False).Name
-            Return New NamedValue(Of String) With {
-                .Value = expr,
-                .Name = name
-            }
-        End Function
-
-        ''' <summary>
-        ''' 这里只是进行解析，并没有立即进行求值
-        ''' </summary>
-        ''' <param name="expr"></param>
-        ''' <returns></returns>
-        Public Function ConstantParser(expr As ScriptToken) As NamedValue(Of String)
-            Return ConstantParser(expr.text)
         End Function
     End Module
 End Namespace
